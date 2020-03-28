ed65ed7d5a6e68f91a9c8e06a869459317d6b37a
Lucciola <hidenari.t1997nov@gmail.com>
2020-03-29 01:20:15 +0900

    add: data module

---
+++ app/src/main/java/jp/co/clockvoid/chaser/di/RepositoryModule.kt
@@ -0,0 +1,13 @@
+package jp.co.clockvoid.repository_impl
+
+import dagger.Binds
+import dagger.Module
+import jp.co.clockvoid.repository.CigaretteRepository
+import javax.inject.Singleton
+
+//@Module
+//abstract class RepositoryModule {
+//    @Binds
+//    @Singleton
+//    abstract fun bindsCigaretteRepository(impl: CigaretteRepositoryImpl): CigaretteRepository
+//}
