836186769812b87cc79cc5ba1c6213eb701cb3ed
Lucciola <hidenari.t1997nov@gmail.com>
2020-05-03 22:31:19 +0900

    add: all items on menu

--- feature/home/src/main/java/jp/co/clockvoid/chaser/feature/home/MainActivity.kt
+++ feature/home/src/main/java/jp/co/clockvoid/chaser/feature/home/MainActivity.kt
@@ -2,6 +2,8 @@ package jp.co.clockvoid.chaser.feature.home
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import dagger.android.support.DaggerAppCompatActivity
 import jp.co.clockvoid.chaser.feature.home.databinding.ActivityMainBinding
 
@@ -9,6 +11,7 @@ class MainActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
+        val binding = DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
+        NavigationUI.setupWithNavController(binding.homeBottomNavigation, findNavController(R.id.navHostFragment))
     }
 }
