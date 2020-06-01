357a5d9aade0071e3326afdd62aa5026bc281f48
clockvoid <hidenari.t1997nov@gmail.com>
2020-06-01 23:38:09 +0900

    add: comment for export schema

--- data/localdatasource-impl/src/main/java/jp/co/clockvoid/chaser/data/localdatasource_impl/CigaretteDatabase.kt
+++ data/localdatasource-impl/src/main/java/jp/co/clockvoid/chaser/data/localdatasource_impl/CigaretteDatabase.kt
@@ -4,6 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
+// TODO: 多分そのうちデータベースをエクスポートするようになるが，一旦incremental buildを有効にするためにexportSchemaをfalseにしている
 @Database(entities = [SmokeEntity::class], version = 1, exportSchema = false)
 @TypeConverters(ZonedDateTimeConverter::class)
 abstract class CigaretteDatabase : RoomDatabase() {
