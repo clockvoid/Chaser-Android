fca7609d3fb8c9e4d8ec56879063dccdd84c2a13
Lucciola <hidenari.t1997nov@gmail.com>
2020-06-17 23:33:05 +0900

    update: kotlin

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -7,7 +7,7 @@ object Dependencies {
     }
 
     object Kotlin {
-        private const val version = "1.3.61"
+        private const val version = "1.3.72"
 
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
