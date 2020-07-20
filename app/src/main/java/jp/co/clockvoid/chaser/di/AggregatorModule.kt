ab17b1bfcfa166ec791275add1bc2c917fb0b0cf
clockvoid <hidenari.t1997nov@gmail.com>
2020-07-20 14:21:27 +0900

    add: aggregatormodule to make hilt recognize disableinstallinchecked modules

---
+++ app/src/main/java/jp/co/clockvoid/chaser/di/AggregatorModule.kt
@@ -0,0 +1,20 @@
+package jp.co.clockvoid.chaser.di
+
+import dagger.Module
+import dagger.android.AndroidInjectionModule
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.migration.DisableInstallInCheck
+import jp.co.clockvoid.chaser.data.localdatasource_impl.LocalDataSourceModule
+import jp.co.clockvoid.chaser.data.repositoryimpl.RepositoryModule
+
+@InstallIn(ApplicationComponent::class)
+@Module(includes = [
+    AppModule::class,
+    AndroidInjectionModule::class,
+    LocalDataSourceModule::class,
+    RepositoryModule::class,
+    MainActivityModule::class,
+    ViewModelModule::class
+])
+interface AggregatorModule
