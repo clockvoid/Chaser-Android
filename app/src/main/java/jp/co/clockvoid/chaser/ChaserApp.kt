4d4a9354f3eaebc89a8e5c9cf2ce2447fec26d4c
clockvoid <hidenari.t1997nov@gmail.com>
2020-07-30 19:29:18 +0900

    delete: dagger android support

--- app/src/main/java/jp/co/clockvoid/chaser/ChaserApp.kt
+++ app/src/main/java/jp/co/clockvoid/chaser/ChaserApp.kt
@@ -2,21 +2,10 @@ package jp.co.clockvoid.chaser
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasAndroidInjector
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
-class ChaserApp : Application(), HasAndroidInjector {
-
-    @Inject
-    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
-
-    override fun androidInjector(): AndroidInjector<Any> {
-        return dispatchingAndroidInjector
-    }
+class ChaserApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/jp/co/clockvoid/chaser/di/AggregatorModule.kt
+++ app/src/main/java/jp/co/clockvoid/chaser/di/AggregatorModule.kt
@@ -1,7 +1,6 @@
 package jp.co.clockvoid.chaser.di
 
 import dagger.Module
-import dagger.android.AndroidInjectionModule
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import jp.co.clockvoid.chaser.data.localdatasource_impl.LocalDataSourceModule
@@ -10,7 +9,6 @@ import jp.co.clockvoid.chaser.data.repositoryimpl.RepositoryModule
 @InstallIn(ApplicationComponent::class)
 @Module(includes = [
     AppModule::class,
-    AndroidInjectionModule::class,
     LocalDataSourceModule::class,
     RepositoryModule::class,
     ViewModelModule::class
