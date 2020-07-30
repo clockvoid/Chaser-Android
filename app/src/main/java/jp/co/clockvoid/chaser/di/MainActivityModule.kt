8fb17f037679337cb603c9340cad184e91892729
clockvoid <hidenari.t1997nov@gmail.com>
2020-07-30 19:25:30 +0900

    delete: cigarettefragmentmodule

--- app/src/main/java/jp/co/clockvoid/chaser/di/MainActivityModule.kt
+++ app/src/main/java/jp/co/clockvoid/chaser/di/MainActivityModule.kt
@@ -3,7 +3,6 @@ package jp.co.clockvoid.chaser.di
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import dagger.hilt.migration.DisableInstallInCheck
-import jp.co.clockvoid.chaser.feature.cigarette.di.CigaretteFragmentModule
 import jp.co.clockvoid.chaser.view.home.MainActivity
 
 @Suppress("unused")
