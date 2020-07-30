ee1414aed9aa845c2b35555facaad49de10eec34
clockvoid <hidenari.t1997nov@gmail.com>
2020-07-31 00:08:35 +0900

    fix: settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
-include(":feature:analytics")
+include()
 rootProject.name="Chaser"
 include(":app",
-    ":feature:cigarette", ":feature:analytics",
+    ":feature:cigarette", ":feature:analytics", ":feature:alcohol",
     ":data:repository-impl", ":data:repository",
     "data:localdatasource-impl", ":data:localdatasource",
     ":core:android", ":core:resources", ":core:model"
