2990654beca13f0c7053b6338823825083a692dd
clockvoid <hidenari.t1997nov@gmail.com>
2020-07-31 21:50:14 +0900

    rename: fragment_analytics to fragment_caffeine and title of the toolbar

--- feature/caffeine/src/main/java/jp/co/clockvoid/chaser/feature/caffeine/CaffeineFragment.kt
+++ feature/caffeine/src/main/java/jp/co/clockvoid/chaser/feature/caffeine/CaffeineFragment.kt
@@ -6,11 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import jp.co.clockvoid.chaser.core.android.extensions.FragmentExtension.dataBinding
-import jp.co.clockvoid.chaser.feature.caffeine.databinding.FragmentAnalyticsBinding
+import jp.co.clockvoid.chaser.feature.caffeine.databinding.FragmentCaffeineBinding
 
 class CaffeineFragment : Fragment() {
 
-    private val binding by dataBinding<FragmentAnalyticsBinding>(R.layout.fragment_analytics)
+    private val binding by dataBinding<FragmentCaffeineBinding>(R.layout.fragment_caffeine)
 
     override fun onCreateView(
         inflater: LayoutInflater,
