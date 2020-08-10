8463f3342a7cf17522cd9e4f576f9f77604abb2b
clockvoid <hidenari.t1997nov@gmail.com>
2020-08-10 21:03:06 +0900

    fix: build.gradle to use kotlin 1.4.0-M3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,26 @@
-include()
+
 rootProject.name="Chaser"
+
 include(":app",
     ":feature:cigarette", ":feature:caffeine", ":feature:alcohol",
     ":data:repository-impl", ":data:repository",
     "data:localdatasource-impl", ":data:localdatasource",
     ":core:android", ":core:resources", ":core:model"
 )
+
+pluginManagement {
+    repositories {
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap/")
+        gradlePluginPortal()
+        jcenter()
+        google()
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.android.application") {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+        }
+    }
+}
