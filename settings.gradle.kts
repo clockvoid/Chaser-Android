911d8ce01e33936bc0ca289d1cb1513c3be1e0f7
clockvoid <hidenari.t1997nov@gmail.com>
2020-08-15 16:22:24 +0900

    apply: kotlin 1.4.0 to build.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,20 +7,3 @@ include(":app",
     "data:localdatasource-impl", ":data:localdatasource",
     ":core:android", ":core:resources", ":core:model"
 )
-
-pluginManagement {
-    repositories {
-        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap/")
-        gradlePluginPortal()
-        jcenter()
-        google()
-    }
-
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "com.android.application") {
-                useModule("com.android.tools.build:gradle:${requested.version}")
-            }
-        }
-    }
-}
