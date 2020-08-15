e172f7484d0e8143d0025ae870adfb0fe0d54f90
clockvoid <hidenari.t1997nov@gmail.com>
2020-08-15 16:22:14 +0900

    update: kotlin to 1.4.0

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -7,7 +7,7 @@ object Dependencies {
     }
 
     object Kotlin {
-        private const val version = "1.3.72"
+        private const val version = "1.4.0"
 
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
