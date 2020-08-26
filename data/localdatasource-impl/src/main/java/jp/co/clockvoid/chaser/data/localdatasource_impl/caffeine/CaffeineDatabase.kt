1d72f63b333784f034c9ae57cb1d6a77b6fb7455
clockvoid <hidenari.t1997nov@gmail.com>
2020-08-26 23:55:37 +0900

    fix: entitiles for caffeinedatabase to fix compiling error

--- data/localdatasource-impl/src/main/java/jp/co/clockvoid/chaser/data/localdatasource_impl/caffeine/CaffeineDatabase.kt
+++ data/localdatasource-impl/src/main/java/jp/co/clockvoid/chaser/data/localdatasource_impl/caffeine/CaffeineDatabase.kt
@@ -4,9 +4,8 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import jp.co.clockvoid.chaser.data.localdatasource_impl.ZonedDateTimeConverter
-import jp.co.clockvoid.chaser.data.localdatasource_impl.cigarette.SmokeEntity
 
-@Database(entities = [SmokeEntity::class], version = 1, exportSchema = false)
+@Database(entities = [BoostEntity::class], version = 1, exportSchema = false)
 @TypeConverters(ZonedDateTimeConverter::class)
 abstract class CaffeineDatabase : RoomDatabase() {
     abstract fun caffeineDao(): CaffeineDao
