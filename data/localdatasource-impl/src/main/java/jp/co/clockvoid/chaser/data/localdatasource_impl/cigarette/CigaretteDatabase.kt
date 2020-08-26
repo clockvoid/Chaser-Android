485c29896640cd352311266cb749d0a919920ba7
clockvoid <hidenari.t1997nov@gmail.com>
2020-08-26 23:20:34 +0900

    move: cigarettedatabases to cigarette package

--- data/localdatasource-impl/src/main/java/jp/co/clockvoid/chaser/data/localdatasource_impl/cigarette/CigaretteDatabase.kt
+++ data/localdatasource-impl/src/main/java/jp/co/clockvoid/chaser/data/localdatasource_impl/cigarette/CigaretteDatabase.kt
@@ -1,8 +1,9 @@
-package jp.co.clockvoid.chaser.data.localdatasource_impl
+package jp.co.clockvoid.chaser.data.localdatasource_impl.cigarette
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import jp.co.clockvoid.chaser.data.localdatasource_impl.ZonedDateTimeConverter
 
 // TODO: 多分そのうちデータベースをエクスポートするようになるが，一旦incremental buildを有効にするためにexportSchemaをfalseにしている
 @Database(entities = [SmokeEntity::class], version = 1, exportSchema = false)
