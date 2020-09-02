d51969c1fd825ca38f60734bdc890afc7ece8ef2
clockvoid <hidenari.t1997nov@gmail.com>
2020-09-02 21:33:14 +0900

    add: timber tree

--- app/src/main/java/jp/co/clockvoid/chaser/ChaserApp.kt
+++ app/src/main/java/jp/co/clockvoid/chaser/ChaserApp.kt
@@ -3,6 +3,7 @@ package jp.co.clockvoid.chaser
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class ChaserApp : Application() {
@@ -10,5 +11,9 @@ class ChaserApp : Application() {
     override fun onCreate() {
         super.onCreate()
         AndroidThreeTen.init(this)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
