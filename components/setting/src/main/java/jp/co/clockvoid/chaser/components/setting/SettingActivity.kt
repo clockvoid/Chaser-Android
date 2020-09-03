13d4e1442c0afd33581ee464a07ff38427fba002
clockvoid <hidenari.t1997nov@gmail.com>
2020-09-03 23:44:19 +0900

    add: logic to initialize setting activity

--- components/setting/src/main/java/jp/co/clockvoid/chaser/components/setting/SettingActivity.kt
+++ components/setting/src/main/java/jp/co/clockvoid/chaser/components/setting/SettingActivity.kt
@@ -1,2 +1,101 @@
 package jp.co.clockvoid.chaser.components.setting
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import jp.co.clockvoid.chaser.components.setting.databinding.ActivitySettingBinding
+
+class SettingActivity : AppCompatActivity(),
+    PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
+
+    private var _binding: ActivitySettingBinding? = null
+    private val binding: ActivitySettingBinding
+        get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolBar)
+        binding.toolBar.setNavigationOnClickListener {
+            finish()
+        }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        } else {
+            title = savedInstanceState.getCharSequence(TITLE_TAG)
+        }
+        supportFragmentManager.addOnBackStackChangedListener {
+            if (supportFragmentManager.backStackEntryCount == 0) {
+                setTitle(R.string.title)
+            }
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        _binding = null
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putCharSequence(TITLE_TAG, title)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        if (supportFragmentManager.popBackStackImmediate()) {
+            return true
+        }
+        return super.onSupportNavigateUp()
+    }
+
+    override fun onPreferenceStartFragment(
+        caller: PreferenceFragmentCompat?,
+        pref: Preference?
+    ): Boolean {
+        if (pref == null) return false
+
+        val args = pref.extras
+        val fragment = supportFragmentManager.fragmentFactory.instantiate(
+            classLoader,
+            pref.fragment
+        ).apply {
+            arguments = args
+            setTargetFragment(caller, 0)
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.settings, fragment)
+            .addToBackStack(null)
+            .commit()
+        title = pref.title
+
+        return true
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.chaser_preferences, rootKey)
+        }
+    }
+
+    companion object {
+        private const val TITLE_TAG = "settingActivityTitle"
+
+        fun from(activity: Activity): Intent {
+            return Intent(activity, SettingActivity::class.java)
+        }
+    }
+}
