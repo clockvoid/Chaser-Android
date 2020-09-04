8696661f4a1c8dce1bbfb43a06498033224623a9
clockvoid <hidenari.t1997nov@gmail.com>
2020-09-04 19:13:49 +0900

    add: preference storage

---
+++ data/repository/src/main/java/jp/co/clockvoid/chaser/data/repository/PreferenceStorage.kt
@@ -0,0 +1,7 @@
+package jp.co.clockvoid.chaser.data.repository
+
+interface PreferenceStorage {
+    var isAlcoholVisible: Boolean
+    var isCaffeineVisible: Boolean
+    var isCigaretteVisible: Boolean
+}
