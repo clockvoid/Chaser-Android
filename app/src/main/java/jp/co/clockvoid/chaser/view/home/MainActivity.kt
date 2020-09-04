9ccc239d4a5569ae61523ad773d39f3c5033d08b
clockvoid <hidenari.t1997nov@gmail.com>
2020-09-04 18:07:12 +0900

    add: controll startdistination

--- app/src/main/java/jp/co/clockvoid/chaser/view/home/MainActivity.kt
+++ app/src/main/java/jp/co/clockvoid/chaser/view/home/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import dagger.hilt.android.AndroidEntryPoint
 import jp.co.clockvoid.chaser.R
 import jp.co.clockvoid.chaser.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -15,11 +17,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = DataBindingUtil.setContentView(this,
+        val binding = DataBindingUtil.setContentView(
+            this,
             R.layout.activity_main
         ) as ActivityMainBinding
-        NavigationUI.setupWithNavController(binding.homeBottomNavigation, findNavController(
-            R.id.navHostFragment
-        ))
+        binding.homeBottomNavigation.menu
+            .removeItem(R.id.cigaretteFragment)
+        binding.homeBottomNavigation.selectedItemId = R.id.alcoholFragment
+        val navHostFragment = navHostFragment as NavHostFragment
+        val inflater = navHostFragment.navController.navInflater
+        val navGraph = inflater.inflate(R.navigation.nav_graph_home).apply {
+            startDestination = R.id.alcoholFragment
+        }
+        navHostFragment.navController.graph = navGraph
+        NavigationUI.setupWithNavController(
+            binding.homeBottomNavigation, findNavController(
+                R.id.navHostFragment
+            )
+        )
     }
 }
