beb898fd90512e982898a1da9000a200a56efe47
clockvoid <hidenari.t1997nov@gmail.com>
2021-01-23 18:32:04 +0900

    fix: chartcontaineritem to display barchart item

--- feature/cigarette/src/main/java/jp/co/clockvoid/chaser/feature/cigarette/ChartContainerItem.kt
+++ feature/cigarette/src/main/java/jp/co/clockvoid/chaser/feature/cigarette/ChartContainerItem.kt
@@ -1,21 +1,27 @@
 package jp.co.clockvoid.chaser.feature.cigarette
 
 import android.view.View
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.viewbinding.BindableItem
-import jp.co.clockvoid.chaser.core.model.Smoke
 import jp.co.clockvoid.chaser.feature.cigarette.databinding.ItemChartContainerBinding
 
-class ChartContainerItem(
-    val items: List<Smoke>
-) : BindableItem<ItemChartContainerBinding>() {
+class ChartContainerItem : BindableItem<ItemChartContainerBinding>() {
 
     override fun bind(binding: ItemChartContainerBinding, position: Int) {
 
+        val adapter = GroupAdapter<GroupieViewHolder>()
+        val items = (0..10).toList().map {
+            BarChartItem(it)
+        }
+        binding.chartContainerViewPager.adapter = adapter
+        adapter.update(items)
     }
 
     override fun getLayout(): Int = R.layout.item_chart_container
 
     override fun initializeViewBinding(view: View): ItemChartContainerBinding {
+
         return ItemChartContainerBinding.bind(view)
     }
 }
